ef56dcf900b3c128f3c18f4e4a44232f3c814b95
PabloArshid <umarsa.birvancity@gmail.com>
2021-02-28 12:07:55 -0800

    search player and get his playerid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import packages
 from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.static import players
 import json
 import requests
 import pandas as pd
@@ -17,10 +18,14 @@ mpl.rcParams['axes.linewidth'] = 2
 
 def main():
     # Draw basketball court
-    fig = plt.figure(figsize=(4, 3.76))
-    ax = fig.add_axes([0, 0, 1, 1])
-    ax = create_court(ax, 'black')
-    plt.show()
+    #fig = plt.figure(figsize=(4, 3.76))
+    #ax = fig.add_axes([0, 0, 1, 1])
+    #ax = create_court(ax, 'black')
+    #plt.show()
+
+    val = input("Who are you looking for: ")
+    lbj = players.find_players_by_full_name(val)
+    print(lbj)
 
 
 if __name__ == '__main__':
